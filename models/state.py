75b3ffc90fe5e623f9055bd3ab304b4755c4472a
Nachocheeze18 <5581@holbertonstudents.com>
2023-03-08 09:16:51 -0800

    updated state class

--- models/state.py
+++ models/state.py
@@ -1,35 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from os import getenv
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
-import models
+from models.city import City
+from sqlalchemy import create_engine, Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class State(BaseModel, Base):
-    '''
-        Implementation for the State.
-        Create relationship between class State (parent) to City (child)
-    '''
+    """Name of table in database to link to"""
     __tablename__ = "states"
 
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                              cascade="all, delete, delete-orphan")
-    else:
-        name = ""
+    """This class defines a user by name & state_id attributes"""
+    name = Column(String(128), nullable=False)
 
-        @property
-        def cities(self):
-            '''
-                Return list of city instances if City.state_id==current
-                State.id
-                FileStorage relationship between State and City
-            '''
-            list_cities = []
-            for city in models.storage.all("City").values():
-                if city.state_id == self.id:
-                    list_cities.append(city)
-            return list_cities
+    cities = relationship("City", backref="state", cascade="delete")
