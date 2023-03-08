69d25e9d29cdfdfdfef052915f3401e557716f74
Nachocheeze18 <5581@holbertonstudents.com>
2023-03-08 09:17:51 -0800

    updated review class

--- models/review.py
+++ models/review.py
@@ -1,21 +1,14 @@
 #!/usr/bin/python3
 """ Review module for the HBNB project """
-from os import getenv
-from sqlalchemy import Column, String, ForeignKey
-from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
+from sqlalchemy import create_engine, Column, String, ForeignKey
 
 
 class Review(BaseModel, Base):
-    '''
-        Implementation for the Review.
-    '''
-    __tablename__ = "reviews"
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        text = Column(String(1024), nullable=False)
-        place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
-        user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
-    else:
-        place_id = ""
-        user_id = ""
-        text = ""
+    """ Name of table in database to link to """
+    __tablename__ = 'reviews'
+
+    """ these classes """
+    text = Column(String(1024), nullable=False)
+    place_id = Column(String(60), ForeignKey("places.id"), nullable=False)
+    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
