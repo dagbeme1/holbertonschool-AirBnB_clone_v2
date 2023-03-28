f8987d0e0065cbaa80b6b7e51cc8dc5a86783589
Dennis Agbemenu <dennisagbedivi1@gmail.com>
2023-03-28 00:44:46 -0700

    state

--- models/state.py
+++ models/state.py
@@ -1,17 +1,37 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from models.city import City
-from sqlalchemy import create_engine, Column, String
-from sqlalchemy.orm import relationship
+'''
+    Implementation of the State class
+'''
 from os import getenv
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.base_model import BaseModel, Base
+import models
 
 
 class State(BaseModel, Base):
-    """Name of table in database to link to"""
+    '''
+        Implementation for the State.
+        Create relationship between class State (parent) to City (child)
+    '''
     __tablename__ = "states"
 
-    """This class defines a user by name & state_id attributes"""
-    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        name = ""
 
-    cities = relationship("City", backref="state", cascade="delete")
+        @property
+        def cities(self):
+            '''
+                Return list of city instances if City.state_id==current
+                State.id
+                FileStorage relationship between State and City
+            '''
+            list_cities = []
+            for city in models.storage.all("City").values():
+                if city.state_id == self.id:
+                    list_cities.append(city)
+            return list_cities
